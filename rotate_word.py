99ca0a0461bc2c52c94e2fb3088c84597b276768
Norbert Wandasz <nwandasz@gmail.com>
2022-08-04 14:34:35 +0200

    Caesar cipher updated

--- rotate_word.py
+++ rotate_word.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python
 import string
 
+
 def rotate_word(str, rate):
     alphabet = string.ascii_lowercase
+    print(alphabet)
+    new_letters = []
     for c in str:
+        #print(alphabet.find(c))
         new_index = alphabet.find(c) + rate
         #print(new_index)
-        if new_index > (len(alphabet)-1):
-            new_index = (alphabet.find(c)-(len(alphabet)-1)) + rate
-            #print(new_index)
-        #print(c)
-       # print(alphabet[new_index])
-        new_string = str.replace(c, alphabet[new_index])
-    return new_string
+        if new_index > (len(alphabet) - 1):
+            new_index = (alphabet.find(c) - (len(alphabet))) + rate
+            # print(new_index)
+        # print(c)
+        # print(alphabet[new_index])
+        new_letters.append(alphabet[new_index])
+    new_word = ''.join(new_letters)
+    return new_word
 
-print(rotate_word('abcdef', 1))
+
+print(rotate_word('melon', -10))
