2f030c2d671e91b6597023679d1dd873f1e57c9e
Norbert Wandasz <nwandasz@gmail.com>
2022-09-15 16:40:36 +0200

    string modyfication and dic creating

--- most_frequent.py
+++ most_frequent.py
@@ -1,12 +1,16 @@
-def most_frequent1(string):
+import string
+
+def most_frequent1(sentence):
+    alphabet = string.ascii_lowercase
+    print(alphabet)
     dictonary = dict()
-    for i in string:
-        count_i = string.count(i)
-        dictonary[i] = count_i
+    sentence_lower = sentence.replace(' ','').lower()
+    for i in sentence_lower:
+        if i in alphabet:
+            count_i = sentence_lower.count(i)
+            dictonary[i] = count_i
 
     return dictonary
 
-print(most_frequent1('a la'))
-
-def most_frequent1(string):
+print(most_frequent1('Ala ma ko%$3ta a kot ma DOM//;;;)'))
 
