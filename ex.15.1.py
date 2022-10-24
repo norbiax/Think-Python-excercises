b887c2acdb32dfc17b1f1dd04ab6ec494cd84bf5
unknown <nwandasz@gmail.com>
2022-10-24 16:02:12 +0200

    ex. 15.1

---
+++ ex.15.1.py
@@ -0,0 +1,14 @@
+class Point:
+    """Represents a point in two-dimensional space
+    """
+
+class Circle:
+    """Represents circle
+    Attributes: center, radius
+    """
+
+circle = Circle()
+circle.radius = 75
+circle.center = Point()
+circle.center.x = 150
+circle.center.y = 100
